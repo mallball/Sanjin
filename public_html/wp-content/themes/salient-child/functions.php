ac7b436b50841f1079beeab062ea3c04bd4fd0a7
Malin <malin@agoodid.se>
2020-01-05 01:43:09 +0100

    another update

--- public_html/wp-content/themes/salient-child/functions.php
+++ public_html/wp-content/themes/salient-child/functions.php
@@ -1,62 +1,75 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
-
-// Remove Additional Information Tab WooCommerce
- 
-add_filter( 'woocommerce_product_tabs', 'remove_info_tab', 98);
-function remove_info_tab($tabs) {
- 
- unset($tabs['additional_information']);
- 
- return $tabs;
-}
- 
-// Remove Reviews Tab WooCommerce
- 
-add_filter( 'woocommerce_product_tabs', 'remove_reviews_tab', 98);
-function remove_reviews_tab($tabs) {
- 
- unset($tabs['reviews']);
- 
- return $tabs;
-}
-
-// Remove Categories Tab WooCommerce
-
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-
-// Remove Category in Breadcrumb WooCommerce
-
-function your_prefix_wc_remove_uncategorized_from_breadcrumb( $crumbs ) {
-	$category 	= get_option( 'default_product_cat' );
-	$caregory_link 	= get_category_link( $category );
-	foreach ( $crumbs as $key => $crumb ) {
-		if ( in_array( $caregory_link, $crumb ) ) {
-			unset( $crumbs[ $key ] );
-		}
-	}
-	return array_values( $crumbs );
-}
-add_filter( 'woocommerce_get_breadcrumb', 'your_prefix_wc_remove_uncategorized_from_breadcrumb' );
-
-
-// Rename Home to Shop in breadcrumb WooCommerce
- 
-add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text' );
-function wcc_change_breadcrumb_home_text( $defaults ) {
-	$defaults['home'] = 'Shop';
-	return $defaults;
-}
-
-// Replace the Home/Shop URL WooCommerce
- 
-add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
-function woo_custom_breadrumb_home_url() {
-    return 'https://sanjin-official.com/3218-2/';
-}
-
-
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+// Remove additional information tab WooCommerce
+add_filter( 'woocommerce_product_tabs', 'remove_info_tab', 98);
+function remove_info_tab($tabs) {
+ 
+ unset($tabs['additional_information']);
+ 
+ return $tabs;
+}
+ 
+// Remove reviews tab WooCommerce
+add_filter( 'woocommerce_product_tabs', 'remove_reviews_tab', 98);
+function remove_reviews_tab($tabs) {
+ 
+ unset($tabs['reviews']);
+ 
+ return $tabs;
+}
+
+
+// Remove categories tab WooCommerce
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
+
+// Remove category in breadcrumb WooCommerce
+function your_prefix_wc_remove_uncategorized_from_breadcrumb( $crumbs ) {
+	$category 	= get_option( 'default_product_cat' );
+	$caregory_link 	= get_category_link( $category );
+	foreach ( $crumbs as $key => $crumb ) {
+		if ( in_array( $caregory_link, $crumb ) ) {
+			unset( $crumbs[ $key ] );
+		}
+	}
+	return array_values( $crumbs );
+}
+add_filter( 'woocommerce_get_breadcrumb', 'your_prefix_wc_remove_uncategorized_from_breadcrumb' );
+
+
+// Remove related products WooCommerce
+function iconic_remove_related_products() {
+    remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+}
+add_action( 'init', 'iconic_remove_related_products', 10 );
+
+
+// Change url in breadcrumb WooCommerce
+add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
+function woo_custom_breadrumb_home_url() {
+    return 'https://sanjin-official.com/shop';
+}
+
+
+// Remove shop from breadcrumb WoooCommerce
+add_filter( 'woocommerce_get_breadcrumb', 'remove_shop_crumb', 20, 2 );
+function remove_shop_crumb( $crumbs, $breadcrumb ){
+    foreach( $crumbs as $key => $crumb ){
+        if( $crumb[0] === __('Shop', 'Woocommerce') ) {
+            unset($crumbs[$key]);
+        }
+    }
+
+    return $crumbs;
+}
+
+add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text' );
+function wcc_change_breadcrumb_home_text( $defaults ) {
+	$defaults['home'] = 'Store';
+	return $defaults;
+}
 ?>
